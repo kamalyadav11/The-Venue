7563a69da01e10b346d245c31f5c3eb373388261
kamal11 <kyzx11@gmail.com>
2019-04-05 14:14:39 +0530

    Header Component

---
+++ src/components/header_footer/Header.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <AppBar
+        position="fixed"
+        style={{ backgroundColor: "#2f2f2f", padding: "10px 0" }}
+      >
+        <Toolbar>
+          <div className="header_logo">
+            <div className="font_righteous header_logo_venue">The Venue</div>
+            <div className="header_logo_title">Scranton City</div>
+          </div>
+          <IconButton
+            aria-label="Menu"
+            color="inherit"
+            onClick={() => alert("hello")}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
